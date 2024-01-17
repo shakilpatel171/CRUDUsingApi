d201d6348eece0b41a30e34b144cc0032cd2574d
Shakil <shakilpatel171@gmail.com>
2024-01-17 13:09:45 +0530

    SQLScript Added

---
+++ Demo/SQLScripts/SQLScript.sql
@@ -0,0 +1,15 @@
+﻿create database Demo
+go 
+use Demo
+go 
+create table Customer
+(
+CustomerId int primary key identity,
+Name varchar(Max),
+City varchar(Max),
+Age int
+)
+go 
+insert into customer values('Happy', 'Pandharpur', 27)
+go
+select * from Customer
