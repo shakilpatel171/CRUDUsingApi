3839fa5871784f8911ba5f822ed864440382e737
shakilpatel171 <99251373+shakilpatel171@users.noreply.github.com>
2026-03-24 22:29:14 +0530

    Create Employee.cs file with Employee model class

---
+++ Demo.Data/Models/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Demo.Data.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
